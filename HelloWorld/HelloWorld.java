2a9dce947aea5831c34c55a382032996001d2a83
MaYi <goopae@163.com>
2017-08-11 19:27:06 +0800

    day11 One12 Version1.0

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,29 +1,77 @@
 /*
-利用字节流将C盘下的a.png图片复制到D盘下(文件名保存一致)
-要求：
-1.一次读写一个字节的方式
+文件复制效率比较
 
-用字节流将C盘下的a.png图片复制到D盘下(文件名保存一致)
-要求：一次读写一个字节数组的方式进行复制
+四种复制文件方式比较
+1.字节流复制文件一次读写一个字节
+2.字节流复制文件一次读写一个字节数组
+3.高效流复制文件一次读写一个字节
+4.高效流复制文件一次读取一个字节数组
+利用上面四种方式复制同一个文件，输出每一种复制方式花费的时间。
  */
 public static void main(String[] args) throws IOException{
-    //一次读写一个字节
-    FileInputStream fis = new FileInputStream("a.png");
-    FileOutputStream fos = new FileOutputStream("b.png");
+    //1.字节流复制文件一次读写一个字节
+    long start = System.currentTimeMillis();
+    ByteOneByOne();
+    long end = System.currentTimeMillis();
+    System.out.println("字节流复制文件一次读写一个字节,花费时间为:"+(end-start)+"ms");
+    //2.字节流复制文件一次读写一个字节数组
+    start = System.currentTimeMillis();
+    ByteOneByArray();
+    end = System.currentTimeMillis();
+    System.out.println("字节流复制文件一次读写一个字节数组,花费时间为:"+(end-start)+"ms");
+    //3.高效流复制文件一次读写一个字节
+    start = System.currentTimeMillis();
+    BufferOneByOne();
+    end = System.currentTimeMillis();
+    System.out.println("高效字节流复制文件一次读写一个字节,花费时间为:"+(end-start)+"ms");
+    //4.高效流复制文件一次读取一个字节数组
+    start = System.currentTimeMillis();
+    BufferOneByArray();
+    end = System.currentTimeMillis();
+    System.out.println("高效字节流复制文件一次读写一个字节数组,花费时间为:"+(end-start)+"ms");
+}
+//1.字节流复制文件一次读写一个字节
+public static void ByteOneByOne() throws IOException{
+    FileInputStream fis = new FileInputStream("a.avi");
+    FileOutputStream fos = new FileOutputStream("b.avi");
     int b;
     while((b = fis.read()) != -1){
         fos.write(b);
     }
     fos.close();
     fis.close();
-    //一次读写一个字节数组
-    FileInputStream fis1 = new FileInputStream("a.png");
-    FileOutputStream fos1 = new FileOutputStream("c.png");
+}
+//2.字节流复制文件一次读写一个字节数组
+public static void ByteOneByArray() throws IOException{
+    FileInputStream fis = new FileInputStream("a.avi");
+    FileOutputStream fos = new FileOutputStream("c.avi");
     int len;
     byte[] bs = new byte[1024];
-    while((len = fis1.read(bs)) != -1){
-        fos1.write(bs,0,len);
+    while((len = fis.read(bs)) != -1){
+        fos.write(bs,0,len);
     }
-    fos1.close();
-    fis1.close();
+    fos.close();
+    fis.close();
+}
+//3.高效流复制文件一次读写一个字节
+public static void BufferOneByOne() throws IOException{
+    BufferedInputStream bis = new BufferedInputStream(new FileInputStream("a.avi"));
+    BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("d.avi"));
+    int b;
+    while((b = bis.read()) != -1){
+        bos.write(b);
+    }
+    bos.close();
+    bis.close();
+}
+public static void BufferOneByArray()throws IOException{
+    BufferedInputStream bis = new BufferedInputStream(new FileInputStream("a.avi"));
+    BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("e.avi"));
+    int len;
+    byte[] bs = new byte[1024];
+    while((len = bis.read(bs)) != -1){
+        bos.write(bs,0,len);
+    }
+    bos.close();
+    bis.close();
 }
