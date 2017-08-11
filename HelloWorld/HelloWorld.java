42460018a2242fdfe844399d41b722b37f42a622
MaYi <goopae@163.com>
2017-08-11 17:36:03 +0800

    day11 One7 Version1.0

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,25 +1,28 @@
 /*
- * FileOutputStream
- * 利用字节输出流一次写一个字节的方式向C盘的a.txt文件输出内容。
- * 利用字节输出流一次写一个字节数组的方式向C盘的b.txt文件输出内容。
+利用字节流将C盘下的a.png图片复制到D盘下(文件名保存一致)
+要求：
+1.一次读写一个字节的方式
+
+用字节流将C盘下的a.png图片复制到D盘下(文件名保存一致)
+要求：一次读写一个字节数组的方式进行复制
  */
 public static void main(String[] args) throws IOException{
-    FileInputStream fis = new FileInputStream("a.txt");
-    FileOutputStream fos = new FileOutputStream("b.txt");
-    //一次写一个字节
+    //一次读写一个字节
+    FileInputStream fis = new FileInputStream("a.png");
+    FileOutputStream fos = new FileOutputStream("b.png");
     int b;
     while((b = fis.read()) != -1){
         fos.write(b);
     }
     fos.close();
     fis.close();
-    //一次写一个字节数组
-    FileInputStream fis1 = new FileInputStream("a.txt");
-    FileOutputStream fos1 = new FileOutputStream("c.txt");
+    //一次读写一个字节数组
+    FileInputStream fis1 = new FileInputStream("a.png");
+    FileOutputStream fos1 = new FileOutputStream("c.png");
     int len;
-    byte[] b1 = new byte[1024];
-    while((len = fis1.read(b1)) != -1){
-        fos1.write(b1,0,len);
+    byte[] bs = new byte[1024];
+    while((len = fis1.read(bs)) != -1){
+        fos1.write(bs,0,len);
     }
     fos1.close();
     fis1.close();
