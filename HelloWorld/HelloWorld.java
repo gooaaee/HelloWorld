84c9d1b935181713ed131cb94c82d6a7fda258e5
MaYi <goopae@163.com>
2017-08-11 17:21:55 +0800

    day11 One2 2.0Version

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -3,9 +3,9 @@
  * 利用字节输出流一次写一个字节的方式向C盘的a.txt文件输出内容。
  * 利用字节输出流一次写一个字节数组的方式向C盘的b.txt文件输出内容。
  */
-public static void main(String[] args){
+public static void main(String[] args) throws IOException{
     FileInputStream fis = new FileInputStream("a.txt");
-    FileOutputStream fos = new FileOutputStream("a.txt");
+    FileOutputStream fos = new FileOutputStream("b.txt");
     //一次写一个字节
     int b;
     while((b = fis.read()) != -1){
@@ -14,13 +14,13 @@ public static void main(String[] args){
     fos.close();
     fis.close();
     //一次写一个字节数组
-    FileInputStream fis = new FileInputStream("a.txt");
-    FileOutputStream fos = new FileOutputStream("b.txt");
+    FileInputStream fis1 = new FileInputStream("a.txt");
+    FileOutputStream fos1 = new FileOutputStream("c.txt");
     int len;
-    byte[] b = new byte[1024];
-    while((len = fis.read(b)) != -1){
-        fos.write(b,0,len);
+    byte[] b1 = new byte[1024];
+    while((len = fis1.read(b1)) != -1){
+        fos1.write(b1,0,len);
     }
-    fos.close();
-    fis.close();
+    fos1.close();
+    fis1.close();
 }
