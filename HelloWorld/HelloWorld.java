e3375e9c46f30b5c1d595f2916ed5d0e3d169c1a
MaYi <goopae@163.com>
2017-08-16 23:01:30 +0800

    HelloWorld.java Version2.0

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,6 +1,6 @@
 /*
 Properties类的使用
-有个文件 名单.txt  里面内容是 
+有个文件 名单.txt  里面内容是
 张三=邯郸
 李四=北京
 王五=襄樊
@@ -8,7 +8,7 @@ Properties类的使用
 钱七=襄樊
 龙八=北京
 读取文件中内容  ,而为了文化,襄樊改为之前的名称 襄阳
-所以需要写一个新文件  新名单.txt,将旧文件中的襄樊替换成襄阳  
+所以需要写一个新文件  新名单.txt,将旧文件中的襄樊替换成襄阳
 提示:先使用集合旧数据读到内存中
    遍历修改
    在将集合写出去
@@ -27,4 +27,3 @@ public static void main(String[] args) throws IOException{
         System.out.println((key + "->" + p.getProperty(key)));
     }
 }
-
